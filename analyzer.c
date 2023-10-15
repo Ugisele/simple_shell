f5981c75e0919cb5c7b120d16932c00a192bd95e
ugisele <umutonigisele12@gmail.com>
2023-10-15 01:58:30 -0700

    Add the analyzer file

---
+++ analyzer.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 
+ */
+int is_cmd(info_typ *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int g = 0, k = 0;
+
+	for (k = 0, g = start; g < stop; g++)
+		if (pathstr[g] != ':')
+			buf[k++] = pathstr[g];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * path_found - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *path_found(info_typ *info, char *pathstr, char *cmd)
+{
+	int g = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[g] || pathstr[g] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, g);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[g])
+				break;
+			curr_pos = g;
+		}
+		g++;
+	}
+	return (NULL);
+}
